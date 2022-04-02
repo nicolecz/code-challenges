84701fd41326dab623d2689752d99aad63282605
Nicole Zach <nczach17@hotmail.com>
2022-04-01 21:09:53 -0500

    Parse int from char code challenge

--- src/com/company/kyu8/Main.java
+++ src/com/company/kyu8/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] myArray = {1,2,3,4};
-        System.out.println(Arrays.toString(LostWithoutAMap.map(myArray)));
+
+        System.out.println(ParseIntFromChar.howOld("0 year old"));
     }
 }
