78f3f10cec889c075a6f3456b30570840e548474
Nicole Zach <nczach17@hotmail.com>
2022-04-01 21:14:49 -0500

    Multiplying two numbers code challenge

--- src/com/company/kyu8/Main.java
+++ src/com/company/kyu8/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(ParseIntFromChar.howOld("0 year old"));
+        System.out.println(MultiplyingTwoNumbers.multiply(2,2));
     }
 }
