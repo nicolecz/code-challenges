517401f268b7428d5d5611991a8e1c37336dec86
Nicole Zach <nczach17@hotmail.com>
2022-04-01 21:41:21 -0500

    If you can't sleep, just count sheep code challenge

--- src/com/company/kyu8/Main.java
+++ src/com/company/kyu8/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(MultiplyingTwoNumbers.multiply(2,2));
+        System.out.println(IfYouCantSleepCountSheep.countingSheep(3));
     }
 }
