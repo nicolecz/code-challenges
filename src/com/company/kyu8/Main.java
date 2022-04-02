d7b040823c7d35c1be93922bcfaaa91cd2dc0821
Nicole Zach <nczach17@hotmail.com>
2022-04-01 20:34:50 -0500

    Lost without a map code challenge

---
+++ src/com/company/kyu8/Main.java
@@ -0,0 +1,11 @@
+package com.company.kyu8;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] myArray = {1,2,3,4};
+        System.out.println(Arrays.toString(LostWithoutAMap.map(myArray)));
+    }
+}
