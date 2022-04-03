22348b6c54713d4f1ed18020fb87ae02ec3e8411
Nicole Zach <nczach17@hotmail.com>
2022-04-02 21:12:47 -0500

    String ends with? code challenge

---
+++ src/com/company/kyu8/StringEndsWith.java
@@ -0,0 +1,13 @@
+package com.company.kyu8;
+
+public class StringEndsWith {
+    public static boolean solution(String str, String ending) {
+        if(str.length() >= ending.length()) {
+            String endOfStr = str.substring(str.length() - ending.length());
+            return endOfStr.equals(ending);
+        }
+        else{
+            return false;
+        }
+    }
+}
