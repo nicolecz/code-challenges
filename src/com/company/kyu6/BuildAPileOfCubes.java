30b5f5b84ec877ec9f7aa997bb0467040e87e5dd
Nicole Zach <nczach17@hotmail.com>
2022-04-12 21:35:31 -0500

    Build a pile of cubes code challenge

---
+++ src/com/company/kyu6/BuildAPileOfCubes.java
@@ -0,0 +1,21 @@
+package com.company.kyu6;
+
+public class BuildAPileOfCubes {
+    public static void main(String[] args) {
+        System.out.println(findNb(1609074227202466816L));
+    }
+    public static long findNb(long m) {
+        long sum = 0;
+        long numberOfCubes = 0;
+        while(sum < m) {
+            numberOfCubes++;
+            sum += (numberOfCubes * numberOfCubes * numberOfCubes);
+        }
+        if(sum == m) {
+            return numberOfCubes;
+        }
+        else {
+            return -1;
+        }
+    }
+}
