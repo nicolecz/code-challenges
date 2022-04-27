7ae601882d2b34fd0f7c6b6ea6544ed21a651bf2
Nicole Zach <nczach17@hotmail.com>
2022-04-27 16:20:53 -0500

    Hidden cubic numbers code challenge

--- src/com/company/kyu7/SumOfCubes.java
+++ src/com/company/kyu7/SumOfCubes.java
@@ -3,7 +3,7 @@ package com.company.kyu7;
 public class SumOfCubes {
 
     public static void main(String[] args) {
-        System.out.println(sumCubes(3));
+        System.out.println(sumCubes(407));
     }
 
     public static long sumCubes(long n) {
