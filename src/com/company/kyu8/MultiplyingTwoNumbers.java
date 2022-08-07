eb7df858878d6a8e7e0cd37ab3352dee169876a4
Nicole Zach <nczach17@hotmail.com>
2022-08-06 22:11:31 -0500

    Count the divisible numbers code challenge, made file Maven project

--- src/com/company/kyu8/MultiplyingTwoNumbers.java
+++
@@ -1,7 +0,0 @@
-package com.company.kyu8;
-
-public class MultiplyingTwoNumbers {
-    public static int multiply(int x,int y) {
-        return x * y;
-    }
-}
--- src/com/company/kyu8/ParseIntFromChar.java
+++
@@ -1,8 +0,0 @@
-package com.company.kyu8;
-
-public class ParseIntFromChar {
-    public static int howOld(final String herOld) {
-        char ageChar = herOld.charAt(0);
-        return Character.getNumericValue(ageChar);
-    }
-}
